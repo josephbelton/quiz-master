b9f3edbe5a0228bd764f564b9dad7528f17a60cf
joebelton <joebelton@live.co.uk>
2020-11-23 16:21:17 +0000

    General Setup - Link frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-// server js
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = 4000
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
