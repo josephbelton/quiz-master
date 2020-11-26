b2e8420fd0cd4f84dfc43800a0e2998a687b180c
joebelton <joebelton@live.co.uk>
2020-11-26 15:46:22 +0000

    Style Login page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { LocationProvider } from '@reach/router';
+import { ThemeProvider } from '@material-ui/styles';
+import theme from './styles/theme';
 
 ReactDOM.render(
   <React.StrictMode>
     <LocationProvider>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </LocationProvider>
   </React.StrictMode>,
   document.getElementById('root')
---
+++ frontend/src/styles/theme.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { createMuiTheme } from '@material-ui/core/styles'
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#ff4400'
+        }
+    }
+})
+
+export default theme;
